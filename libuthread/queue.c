0ddc3283e242f84c6d76618861882c22461924f2
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-08 16:22:54 -0700

    Update queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -37,11 +37,12 @@ queue_t queue_create(void)
  *
  * Return: Pointer to new empty queue. NULL in case of failure when allocating
  * the new queue.
- */
+*/
 	queue_t created_queue = (queue_t)malloc(sizeof(struct queue));
 	created_queue->queue_size = 0;
 	created_queue->first = NULL;
 	created_queue->last = NULL;
+	 
 	if(perror() == -1){
 		return NULL;
 	}
@@ -103,23 +104,61 @@ int queue_dequeue(queue_t queue, void **data)
  * Return: -1 if @queue or @data are NULL, or if the queue is empty. 0 if @data
  * was set with the oldest item available in @queue.
  */
+	if(queue == NULL || data == NULL || queue_length(queue) == 0){
+		return -1;
+	}
 	element_t element_to_be_dequeued;	
+	element_to_be_dequeued = queue->first;
 	*data = element_to_be_dequeued->element_data_address;
-
+	queue->first = element_to_be_dequeued->next_element;
+	queue->queue_size -= 1;
+	return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+/*
+ * queue_func_t - Queue callback function type
+ * @queue: Queue to which item belongs
+ * @data: Data item
+ *
+ * Function to be run on each item using queue_iterate(). The current item is
+ * received as @data.
+ */
+
 }
 
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	/* TODO Phase 1 */
+/*
+ * queue_iterate - Iterate through a queue
+ * @queue: Queue to iterate through
+ * @func: Function to call on each queue item
+ *
+ * This function iterates through the items in the queue @queue, from the oldest
+ * item to the newest item, and calls the given callback function @func on each
+ * item. The callback function receives the current data item as parameter.
+ *
+ * Note that this function should be resistant to data items being deleted
+ * as part of the iteration (ie in @func).
+ *
+ * Return: -1 if @queue or @func are NULL, 0 otherwise.
+ */
 }
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+/*
+ * queue_length - Queue length
+ * @queue: Queue to get the length of
+ *
+ * Return the length of queue @queue.
+ *
+ * Return: -1 if @queue is NULL. Length of @queue otherwise.
+ */
+	if(queue == NULL){
+		return -1;
+	}
+	return queue->queue_size;
 }
 
