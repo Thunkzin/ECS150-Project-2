d90a87d1b28f0d3609ce5ac10f3a43bb55ef14ee
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-09 15:28:05 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -10,6 +10,18 @@
 #include "uthread.h"
 #include "queue.h"
 
+
+
+struct uthread_tcb *current;
+queue_t alive_thread_queue;
+queue_t zombie_thread_queue;
+
+struct uthread_tcb {
+	struct state thread_state;
+	void* stack;
+	uthread_ctx_t *thread_context;
+};
+
 struct state{
 	bool running;
 	bool ready;
@@ -17,10 +29,7 @@ struct state{
 	bool zombie;
 };
 
-struct uthread_tcb {
-	void* stack;
-};
-struct uthread_tcb *current;
+
 
 struct uthread_tcb *uthread_current(void)
 {
@@ -35,6 +44,8 @@ void uthread_yield(void)
  * This function is to be called from the currently active and running thread in
  * order to yield for other threads to execute.
  */
+
+
 }
 
 void uthread_exit(void)
@@ -81,7 +92,38 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
  * Return: 0 in case of success, -1 in case of failure (e.g., memory allocation,
  * context creation).
  */
+	if(preempt){
+		preempt_enable();
+	}
+	struct uthread_tcb *idle_thread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
+	if(idle_thread == NULL){
+		return -1;
+	}
+	current = idle_thread;
+	//set the current stack's state
+	current->thread_state.running = true;
+	current->thread_state.ready = false;
+	current->thread_state.blocked = false;
+	current->thread_state.zombie = false;
+	//allocate current stack's memory 
+	current->stack = uthread_ctx_alloc_stack;
+	//initialize the thread's context
+	current->thread_context = (uthread_ctx_t *)malloc(sizeof(uthread_ctx_t));
+	uthread_ctx_init(current->thread_context, current->stack, func, arg);
 
+
+	while(alive_thread_queue != 0){
+		while(zombie_thread_queue != 0){
+			struct uthread_tcb *zombie_thread;
+			//move the dequeued data into zombie_thread's context, then remove the thread
+			queue_dequeue(zombie_thread_queue, zombie_thread->thread_context);
+			free(zombie_thread->thread_context);
+			uthread_ctx_destroy_stack(zombie_thread->stack);
+		}
+		//keep running different thread.
+		uthread_yield();
+	}
+	return 0;
 }
 
 void uthread_block(void)
