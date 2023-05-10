994b174edcd10a258142b2a02e99976ee33b4ad1
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-10 15:34:54 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -112,14 +112,12 @@ int uthread_create(uthread_func_t func, void *arg)
 	new_thread->stack = uthread_ctx_alloc_stack();
 	//initialize the thread's context
 	uthread_ctx_init(new_thread->thread_context, new_thread->stack, func, arg);
-	printf("115 alive_thread_queue_length:%i\n", queue_length(alive_thread_queue));
 	//enqueue the thread into the queue
 	queue_enqueue(alive_thread_queue, new_thread);
 
 	if(new_thread == NULL){
 		return -1;
 	}
-	printf("122 alive_thread_queue_length:%i\n", queue_length(alive_thread_queue));
 	return 0;
 }
 
@@ -141,14 +139,14 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
  * Return: 0 in case of success, -1 in case of failure (e.g., memory allocation,
  * context creation).
  */
-
+	alive_thread_queue = queue_create();
+	zombie_thread_queue = queue_create();
 
 	if(preempt){
 		int i = 0;
 		i++;
 	}
 
-	printf("150 alive_thread_queue_length:%i\n", queue_length(alive_thread_queue));
 
 	//create the idle_thread
 	struct uthread_tcb *idle_thread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
@@ -171,6 +169,7 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 
 	// create the very first thread into the alive queue
 	uthread_create(func, arg);
+	printf("172 alive_thread_queue_length:%i\n", queue_length(alive_thread_queue));
 
 	while(queue_length(alive_thread_queue) > 0){
 		while(queue_length(zombie_thread_queue) > 0){
