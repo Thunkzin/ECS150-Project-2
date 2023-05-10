361b790ee50f601132ddafab1e1828f7a0a4e59b
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-09 20:36:44 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -170,7 +170,7 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 		while(zombie_thread_queue != 0){
 			struct uthread_tcb *zombie_thread;
 			//move the dequeued data into zombie_thread's context, then remove the thread
-			queue_dequeue(zombie_thread_queue, (void**)&zombie_thread);
+			queue_dequeue(zombie_thread_queue, (void**)zombie_thread->thread_context);
 			free(zombie_thread->thread_context);
 			uthread_ctx_destroy_stack(zombie_thread->stack);
 		}
