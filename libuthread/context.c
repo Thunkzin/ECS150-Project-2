bfab656f17a8a66af887da3ce37ecff3fe0418b1
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-09 20:23:53 -0700

    Update context.c

--- libuthread/context.c
+++ libuthread/context.c
@@ -3,7 +3,7 @@
 
 #include "private.h"
 #include "uthread.h"
-#include "preempt.c"
+
 
 /* Size of the stack for a thread (in bytes) */
 #define UTHREAD_STACK_SIZE 32768
@@ -40,6 +40,8 @@ static void uthread_ctx_bootstrap(uthread_func_t func, void *arg)
 	/*
 	 * Enable interrupts right after being elected to run for the first time
 	 */
+	
+	
 	//preempt_enable();
 
 	/* Execute thread and when done, exit */
