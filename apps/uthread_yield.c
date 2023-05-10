1e27e59daf8306f221a1f5bc47994220ccffeb90
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-09 19:26:56 -0700

    Update uthread_yield.c

--- apps/uthread_yield.c
+++ apps/uthread_yield.c
@@ -10,7 +10,6 @@
  * thread3
  */
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,16 +17,12 @@
 
 void thread3(void *arg)
 {
-	(void)arg;
-
 	uthread_yield();
 	printf("thread3\n");
 }
 
 void thread2(void *arg)
 {
-	(void)arg;
-
 	uthread_create(thread3, NULL);
 	uthread_yield();
 	printf("thread2\n");
@@ -35,8 +30,6 @@ void thread2(void *arg)
 
 void thread1(void *arg)
 {
-	(void)arg;
-
 	uthread_create(thread2, NULL);
 	uthread_yield();
 	printf("thread1\n");
@@ -45,6 +38,6 @@ void thread1(void *arg)
 
 int main(void)
 {
-	uthread_run(false, thread1, NULL);
+	uthread_run(thread1, NULL);
 	return 0;
 }
