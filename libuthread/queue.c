dd37512eb09be7788cbde40a81256165dfe75568
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-09 21:40:59 -0700

    Update queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,238 +1,199 @@
+#include <assert.h>
+#include <signal.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include <sys/time.h>
 
+
+#include "private.h"
+#include "uthread.h"
 #include "queue.h"
+//#include "preempt.c"
 
-typedef struct element* element_t;
 
-struct element{
-	void *element_data_address;
-	struct element *next_element;
+
+struct uthread_tcb *current;
+queue_t alive_thread_queue;
+queue_t zombie_thread_queue;
+
+struct uthread_tcb {
+	enum { running, ready, blocked, zombie} thread_state;
+	void* stack;
+	uthread_ctx_t *thread_context;
 };
 
 
-struct queue {
-/*
- * queue_t - Queue type
- *
- * A queue is a FIFO data structure. Data items are enqueued one after the
- * other.  When dequeueing, the queue must returned the oldest enqueued item
- * first and so on.
- *
- * Apart from delete and iterate operations, all operations should be O(1).
- */
-	int queue_size;
-	element_t first;
-	element_t last;
-};
 
-queue_t queue_create(void)
-{
-/*
- * queue_create - Allocate an empty queue
- *
- * Create a new object of type 'struct queue' and return its address.
- *
- * Return: Pointer to new empty queue. NULL in case of failure when allocating
- * the new queue.
-*/
-	queue_t created_queue = (queue_t)malloc(sizeof(struct queue));
-	created_queue->queue_size = 0;
-	created_queue->first = NULL;
-	created_queue->last = NULL;
-	return created_queue;
+
+struct uthread_tcb *uthread_current(void){
+	return current;
 }
 
-int queue_destroy(queue_t queue)
+void uthread_yield(void)
 {
 /*
- * queue_destroy - Deallocate a queue
- * @queue: Queue to deallocate
+ * uthread_yield - Yield execution
  *
- * Deallocate the memory associated to the queue object pointed by @queue.
- *
- * Return: -1 if @queue is NULL or if @queue is not empty. 0 if @queue was
- * successfully destroyed.
+ * This function is to be called from the currently active and running thread in
+ * order to yield for other threads to execute.
  */
-	if(queue == NULL || queue->first != NULL || queue->last != NULL){
+
+	if(current->thread_state == running){
+		//set the currently running thread's state into ready
+		current->thread_state = ready;
+		//put the ready thread into the alive_queue to wait for next run
+		queue_enqueue(alive_thread_queue, current);
+	}
+
+	struct uthread_tcb *popped_out_thread = NULL;
+	//pop out the oldest thread from the queue, and store it into popped_out_thread
+	if(queue_dequeue(alive_thread_queue, (void**)popped_out_thread) != 0){
+		//if error occurs, return
+		return;
+	}
+	struct uthread_tcb *current_thread_snap_shot = uthread_current();
+
+	if(popped_out_thread->thread_state == ready){
+		//set the popped_out_thread into running
+		popped_out_thread->thread_state = running;	
+		//turn current thread into the popped_out_thread (the snap shot is taken so it's fine)
+		current = popped_out_thread;
+		//switch the context between thread_snap_shot and popped_out_thread.
+		uthread_ctx_switch(current_thread_snap_shot->thread_context, popped_out_thread->thread_context);
+	}
+
+}
+
+void uthread_exit(void)
+{
+/*
+ * uthread_exit - Exit from currently running thread
+ *
+ * This function is to be called from the currently active and running thread in
+ * order to finish its execution.
+ *
+ * This function shall never return.
+ */
+	perror("Exiting current thread.\n");
+	struct uthread_tcb *exiting_thread;
+	exiting_thread = uthread_current();
+	/* set the current thread to zombie when exit func is called */
+	exiting_thread->thread_state = zombie;
+	/* enqueue the current thread onto zombie_thread_queue */
+	queue_enqueue(zombie_thread_queue,exiting_thread);
+	/* yield */
+	uthread_yield();
+
+	/* trigger an error and terminate in case current thread continue to execute after call to unthread_exit  */
+	assert(false);
+
+}
+
+int uthread_create(uthread_func_t func, void *arg)
+{
+/*
+ * uthread_create - Create a new thread
+ * @func: Function to be executed by the thread
+ * @arg: Argument to be passed to the thread
+ *
+ * This function creates a new thread running the function @func to which
+ * argument @arg is passed.
+ *
+ * Return: 0 in case of success, -1 in case of failure (e.g., memory allocation,
+ * context creation).
+ */
+	struct uthread_tcb *new_thread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
+	new_thread->thread_context = (uthread_ctx_t *)malloc(sizeof(uthread_ctx_t));
+	//set the current stack's state
+	new_thread->thread_state = ready;
+	//allocate current stack's memory 
+	new_thread->stack = uthread_ctx_alloc_stack();
+	//initialize the thread's context
+	uthread_ctx_init(new_thread->thread_context, new_thread->stack, func, arg);
+	printf("115 alive_thread_queue_length:%i", queue_length(alive_thread_queue));
+	//enqueue the thread into the queue
+	queue_enqueue(alive_thread_queue, new_thread);
+
+	if(new_thread == NULL){
 		return -1;
 	}
-	free(queue);
+	printf("122 alive_thread_queue_length:%i", queue_length(alive_thread_queue));
 	return 0;
 }
 
-int queue_enqueue(queue_t queue, void *data)
+int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
 /*
- * queue_enqueue - Enqueue data item
- * @queue: Queue in which to enqueue item
- * @data: Address of data item to enqueue
+ * uthread_run - Run the multithreading library
+ * @preempt: Preemption enable
+ * @func: Function of the first thread to start
+ * @arg: Argument to be passed to the first thread
  *
- * Enqueue the address contained in @data in the queue @queue.
+ * This function should only be called by the process' original execution
+ * thread. It starts the multithreading scheduling library, and becomes the
+ * "idle" thread. It returns once all the threads ha
+ * ve finished running.
  *
- * Return: -1 if @queue or @data are NULL, or in case of memory allocation error
- * when enqueing. 0 if @data was successfully enqueued in @queue.
+ * If @preempt is `true`, then preemptive scheduling is enabled.
+ *
+ * Return: 0 in case of success, -1 in case of failure (e.g., memory allocation,
+ * context creation).
  */
-	if(queue == NULL || data == NULL){
-		return -1;
+
+	if(preempt){
+		int i = 0;
+		i++;
 	}
-	element_t element_to_be_enqueued = (element_t)malloc(sizeof(struct element));
-	element_to_be_enqueued->element_data_address = data;
-	element_to_be_enqueued->next_element = NULL;
-	if(queue->queue_size == 0){
-		/*If the item to be enqueue is the first item*/
-		queue->first = element_to_be_enqueued;
-		queue->last = element_to_be_enqueued;
-	}else{
-		queue->last->next_element = element_to_be_enqueued;
-		queue->last = element_to_be_enqueued;
+
+
+	//create the idle_thread
+	struct uthread_tcb *idle_thread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
+	if(idle_thread == NULL){
+		return -1;
 	}
 	
-	queue->queue_size ++;
-	return 0;
-}
+	//initialize the thread's context
+	idle_thread->thread_context = (uthread_ctx_t *)malloc(sizeof(uthread_ctx_t));
 
-int queue_dequeue(queue_t queue, void **data)
-{
-/*
- * queue_dequeue - Dequeue data item
- * @queue: Queue in which to dequeue item
- * @data: Address of data pointer where item is received
- *
- * Remove the oldest item of queue @queue and assign this item (the value of a
- * pointer) to @data.
- *
- * Return: -1 if @queue or @data are NULL, or if the queue is empty. 0 if @data
- * was set with the oldest item available in @queue.
- */
-	if(queue == NULL || data == NULL || queue_length(queue) == 0){
-		return -1;
-	}
-	element_t element_to_be_dequeued = (element_t)malloc(sizeof(struct element));
-	if(queue_length(queue) == 1){
-		element_to_be_dequeued = queue->first;
-		*data = element_to_be_dequeued->element_data_address;
-		queue->first = NULL;
-		queue->last = NULL;
-		queue->queue_size = 0;
-	}else{
-		element_to_be_dequeued = queue->first;
-		*data = element_to_be_dequeued->element_data_address;
-		queue->first = element_to_be_dequeued->next_element;
-		queue->queue_size -= 1;
-	}
-	return 0;
-}
 
-int queue_delete(queue_t queue, void *data)
-{
-/*
- * queue_delete - Delete data item
- * @queue: Queue in which to delete item
- * @data: Data to delete
- *
- * Find in queue @queue, the first (ie oldest) item equal to @data and delete
- * this item.
- *
- * Return: -1 if @queue or @data are NULL, of if @data was not found in the
- * queue. 0 if @data was found and deleted from @queue.
- */
-	if(queue == NULL || data == NULL){
-        	return -1;
-  	  }
 
-	element_t current = queue->first;
-	element_t previous = NULL;
-	int current_node_number = 0;
-	int found = 0;
+	//set the current stack's state
+	idle_thread->thread_state = running;
+	current = idle_thread;
+	//allocate current stack's memory 
+	current->stack = uthread_ctx_alloc_stack();
 
-	/* iterate through the queue to find the data */
-	while (current_node_number < queue_length(queue)) {
-		if(current->element_data_address == data){
-			found = 1;
-			break;
+	uthread_ctx_init(current->thread_context, current->stack, func, arg);
+
+	// create the very first thread into the alive queue
+	uthread_create(func, arg);
+
+	while(queue_length(alive_thread_queue) > 0){
+		while(queue_length(zombie_thread_queue) > 0){
+			struct uthread_tcb *zombie_thread;
+			//move the dequeued data into zombie_thread's context, then remove the thread
+			queue_dequeue(zombie_thread_queue, (void**)&zombie_thread);
+			free(zombie_thread->thread_context);
+			uthread_ctx_destroy_stack(zombie_thread->stack);
 		}
-		/* move to next node */
-		previous = current;
-		current = current->next_element;
-		current_node_number++;
-
-	}
-	if(!found){
-		return -1;
-	}
-	/* delete the node with matching data */
-	/* current is at first node */
-	if(previous == NULL){
-		queue->first = current->next_element; // the node next to current become first node.
-	}
-	/* current is not at first node and it's somewhere in between */
-	else {
-		previous->next_element = current->next_element;
-	}
-	/* if current is at the last node */
-	if (current == queue->last) {
-		queue->last = previous;
-	}
-	queue->queue_size -= 1;
-	free(current);
-	return 0;
-}
-
-
-int queue_iterate(queue_t queue, queue_func_t func)
-{
-/*
- * queue_iterate - Iterate through a queue
- * @queue: Queue to iterate through
- * @func: Function to call on each queue item
- *
- * This function iterates through the items in the queue @queue, from the oldest
- * item to the newest item, and calls the given callback function @func on each
- * item. The callback function receives the current data item as parameter.
- *
- * Note that this function should be resistant to data items being deleted
- * as part of the iteration (ie in @func).
- *
- * Return: -1 if @queue or @func are NULL, 0 otherwise.
- */
-	if(queue == NULL || func == NULL){
-		return -1;
-	}
-	element_t current_element = queue->first;
-	for(int current_element_number = 0; current_element_number < queue_length(queue); ++current_element_number){
-		func(queue, current_element->element_data_address);
-		current_element = current_element->next_element;
+		//keep running different thread.
+		uthread_yield();
 	}
 	return 0;
 }
-/*
- * typedef void (*queue_func_t)(queue_t queue, void *data);
 
- * queue_func_t - Queue callback function type
- * @queue: Queue to which item belongs
- * @data: Data item
- *
- * Function to be run on each item using queue_iterate(). The current item is
- * received as @data.
- */
+// void uthread_block(void)
+// {
+// 	current->thread_state = blocked;
+// }
 
-
-int queue_length(queue_t queue)
-{
-/*
- * queue_length - Queue length
- * @queue: Queue to get the length of
- *
- * Return the length of queue @queue.
- *
- * Return: -1 if @queue is NULL. Length of @queue otherwise.
- */
-	if(queue == NULL){
-		return -1;
-	}
-	return queue->queue_size;
-}
-
+// void uthread_unblock(struct uthread_tcb *uthread)
+// {
+// 	preempt_enable();
+// 	preempt_disable();
+// 	queue_enqueue(alive_thread_queue, uthread);
+// 	preempt_enable();
+// 	return;
+// }
