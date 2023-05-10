e142000bc7e287365dec71d736c412a53a28c9dc
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-09 20:57:49 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -170,8 +170,8 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 	// create the very first thread into the alive queue
 	uthread_create(func, arg);
 
-	while(alive_thread_queue != 0){
-		while(zombie_thread_queue != 0){
+	while(alive_thread_queue != NULL){
+		while(zombie_thread_queue != NULL){
 			struct uthread_tcb *zombie_thread;
 			//move the dequeued data into zombie_thread's context, then remove the thread
 			queue_dequeue(zombie_thread_queue, (void**)&zombie_thread);
