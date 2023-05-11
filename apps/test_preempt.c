c99fe0570a79b795a2204890dd7a1e941a2f9e0d
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-10 22:44:48 -0700

    Create test_preempt.c

---
+++ apps/test_preempt.c
@@ -0,0 +1,38 @@
+ /*
+    Simple test for preempt
+ */
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <uthread.h>
+#include <preempt.c>
+#include <private.h>
+
+
+
+
+void thread2(void *arg)
+{
+	(void)arg;
+	uthread_yield();
+	printf("thread2\n");
+}
+
+void thread1(void *arg)
+{
+	(void)arg;
+    uthread_create(thread2, NULL);
+    uthread_block();
+	uthread_yield();
+	printf("thread1\n");
+	uthread_yield();
+    uthread_unblock();
+}
+
+int main(void)
+{
+    
+	uthread_run(true, thread1, NULL);
+	return 0;
+}
