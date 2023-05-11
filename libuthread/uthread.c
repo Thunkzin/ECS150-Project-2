33ae3431d67299438668fba4b65533b59225d368
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-10 22:38:28 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -38,17 +38,18 @@ void uthread_yield(void)
  * This function is to be called from the currently active and running thread in
  * order to yield for other threads to execute.
  */
+	struct uthread_tcb *popped_out_thread;
 
 	if(current->thread_state == running){
 		//set the currently running thread's state into ready
 		current->thread_state = ready;
 		//put the ready thread into the alive_queue to wait for next run
 		queue_enqueue(alive_thread_queue, current);
+		queue_dequeue(alive_thread_queue, (void**)&popped_out_thread);
+
 	}
 
-	struct uthread_tcb *popped_out_thread;
 	//pop out the oldest thread from the queue, and store it into popped_out_thread
-	queue_dequeue(alive_thread_queue, (void**)&popped_out_thread);
 	struct uthread_tcb *current_thread_snap_shot = uthread_current();
 	if(popped_out_thread->thread_state == ready){
 		//set the popped_out_thread into running
