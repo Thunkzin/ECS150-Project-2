70616f177f643f2ad97dd2b938de577d53df40b4
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-05-11 17:21:40 -0700

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -10,7 +10,6 @@
 #include "private.h"
 #include "uthread.h"
 
-
 /*
  * Frequency of preemption
  * 100Hz is 100 times per second
@@ -24,90 +23,92 @@ struct itimerval old;
 
 bool enable_preempt = false;
 
-void signal_handler(){
+void signal_handler()
+{
     uthread_yield();
 }
 
 /* this func can block signals of type SIGVTALRM */
 void preempt_disable(void)
 {
-	/* Create a signal set to hold the mask of blocked signals */
-
-    /* Initialize the signal mask to be empty */
-    sigemptyset(&signal_action.sa_mask);
-
-    /* Add the SIGVTALRM signal to the signal set */
-    sigaddset(&signal_action.sa_mask, SIGVTALRM);
-
-    /* Block the SIGVTALRM signal in the current thread */
-    sigprocmask(SIG_BLOCK, &signal_action.sa_mask, &old_signal_action.sa_mask);
-
     // Disable preemption only if it is currently enabled
-    if (enable_preempt) {
+    if (enable_preempt)
+    {
         enable_preempt = false;
+        /* Initialize the signal mask to be empty */
+        sigemptyset(&signal_action.sa_mask);
+
+        /* Add the SIGVTALRM signal to the signal set */
+        sigaddset(&signal_action.sa_mask, SIGVTALRM);
+
+        /* Block the SIGVTALRM signal in the current thread */
+        sigprocmask(SIG_BLOCK, &signal_action.sa_mask, &old_signal_action.sa_mask);
     }
 }
 
 /* this func can unblock signals of type SIGVTALRM */
 void preempt_enable(void)
 {
-	// Create a signal set to hold the mask of unblocked signals
-
-
-    // Initialize the signal set to be empty
-    sigemptyset(&signal_action.sa_mask);
-
-    // Add the SIGVTALRM signal to the signal set
-    sigaddset(&signal_action.sa_mask, SIGVTALRM);
-
-    // Unblock the SIGVTALRM signal in the current thread
-    sigprocmask(SIG_UNBLOCK, &signal_action.sa_mask, &old_signal_action.sa_mask);
-
     // Enable preemption only if it is currently disabled
-    if (!enable_preempt) {
+    if (!enable_preempt)
+    {
         enable_preempt = true;
+        // Initialize the signal set to be empty
+        sigemptyset(&signal_action.sa_mask);
+
+        // Add the SIGVTALRM signal to the signal set
+        sigaddset(&signal_action.sa_mask, SIGVTALRM);
+
+        // Unblock the SIGVTALRM signal in the current thread
+        sigprocmask(SIG_UNBLOCK, &signal_action.sa_mask, &old_signal_action.sa_mask);
     }
 }
 
-
 void preempt_start(bool preempt)
 {
-/**
- * Private preemption API
- */
+    /**
+     * Private preemption API
+     */
 
-/*
- * preempt_start - Start thread preemption
- * @preempt: Enable preemption if true
- *
- * Configure a timer that must fire a virtual alarm at a frequency of 100 Hz and
- * setup a timer handler that forcefully yields the currently running thread.
- *
- * If @preempt is false, don't start preemption; all the other functions from
- * the preemption API should then be ineffective.
- */
+    /*
+     * preempt_start - Start thread preemption
+     * @preempt: Enable preemption if true
+     *
+     * Configure a timer that must fire a virtual alarm at a frequency of 100 Hz and
+     * setup a timer handler that forcefully yields the currently running thread.
+     *
+     * If @preempt is false, don't start preemption; all the other functions from
+     * the preemption API should then be ineffective.
+     */
 
-	/* if preempt is enabled */
-	if (preempt) {
+    standard.it_value.tv_usec = MICROSEC;
+    standard.it_value.tv_sec = 0;
+    standard.it_interval.tv_usec = MICROSEC;
+    standard.it_interval.tv_sec = 0;
+    /* if preempt is enabled */
+    if (preempt)
+    {
         preempt_enable();
         signal_action.sa_handler = signal_handler;
-        sigaction(SIGALRM, &signal_action, &old_signal_action);
-        ualarm(MICROSEC, 0);
+        sigemptyset(&signal_action.sa_mask);
+        sigaddset(&signal_action.sa_mask, SIGVTALRM);
+        signal_action.sa_flags = 0;
+        sigaction(SIGVTALRM, &signal_action, &old_signal_action);
+        setitimer(ITIMER_VIRTUAL, &standard, NULL);
     }
-
 }
 
 void preempt_stop(void)
 {
-/* TODO Phase 4 */
-/*
- * preempt_stop - Stop thread preemption
- *
- * Restore previous timer configuration, and previous action associated to
- * virtual alarm signals.
- */
+    /* TODO Phase 4 */
+    /*
+     * preempt_stop - Stop thread preemption
+     *
+     * Restore previous timer configuration, and previous action associated to
+     * virtual alarm signals.
+     */
     // getitimer(ITIMER_VIRTUAL, );
     preempt_disable();
     signal_action = old_signal_action;
+    standard = old;
 }
-
